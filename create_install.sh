fd05ce023aa14499b53d62500716db500728da81
palmermarc <palmermarc@gmail.com>
2018-05-08 10:48:03 -0500

    Updated script
    Updated the script to include the most recent changes that has been
    made.

--- create_install.sh
+++ create_install.sh
@@ -2,10 +2,8 @@
 
 #Generic vars for later
 INSTALLPATH="/Library/WebServer/Documents"
-
-#This is the local user you're logged in as
-#Only needed if you're going to use WP CLI
-USER="LOCALUSER"
+USER=""
+serveradmin=""
 
 #DB vars needed for later
 DBHOST="127.0.0.1"
@@ -19,34 +17,34 @@ WPTITLE=""
 
 echo -e "Please provide an install name."
 read installname
+DEVDOMAIN=$installname.local
 
-mkdir $INSTALLPATH/$installname
-mkdir $INSTALLPATH/$installname/public_html
-mkdir $INSTALLPATH/$installname/logs
+echo -e "Which folder should this be placed in? (stlclients,hubbard,2060digital)"
+read FOLDER
+mkdir $INSTALLPATH/$FOLDER/$installname
+mkdir $INSTALLPATH/$FOLDER/$installname/public_html
+mkdir $INSTALLPATH/$FOLDER/$installname/logs
 
-chmod -R 755 $INSTALLPATH/$installname
+chmod -R 777 $INSTALLPATH/$FOLDER/$installname
 
-echo -e "Please provide the dev domain"
-read devdomain
-
-echo -e "127.0.0.1	$devdomain" >> /etc/hosts
+echo -e "127.0.0.1	$DEVDOMAIN" >> /etc/hosts
 
 echo "Let's begin creating your vhost file now."
 
-echo -e "Please enter the ServerAdmin's email address"
-read serveradmin
-
 touch /etc/apache2/other/httpd-$installname.conf
 echo -e "<VirtualHost *:80>
-	ServerAdmin $serveradmin
-        DocumentRoot \"$INSTALLPATH/$installname/public_html\"
-        ServerName $devdomain
-	ServerAlias www.$devdomain
-	ErrorLog $INSTALLPATH/$installname/logs/error_log
-	CustomLog \"$INSTALLPATH/$installname/logs/access_log\" common
+	ServerAdmin mapalmer@hbi.com
+  DocumentRoot \"$INSTALLPATH/$FOLDER/$installname/public_html\"
+  ServerName $DEVDOMAIN
+	ServerAlias www.$DEVDOMAIN
+	ErrorLog $INSTALLPATH/$FOLDER/$installname/logs/error_log
+	CustomLog \"$INSTALLPATH/$FOLDER/$installname/logs/access_log\" common
 </VirtualHost>" >> /etc/apache2/other/httpd-$installname.conf
 
 echo "Finished created your vhost file."
+
+apachectl restart
+echo "Apache has been restarted."
 
 echo "Do we need to create a database for this install?"
 read cdb
@@ -58,16 +56,18 @@ echo "Are you wanting to create a fresh install with WP-CLI?"
 read usewpcli
 if [ $usewpcli == 'yes' ]; then
 	#without this, download failed for me. Remove at your own risk
-	chmod -R 777 $INSTALLPATH/$installname
-	
+	chmod -R 777 $INSTALLPATH/$FOLDER/$installname
+
 	#Running this as sudo barks an error, so we need to tell sudo to use the local user
-	sudo -u $USER -i -- wp core download --path=$INSTALLPATH/$installname/public_html	
-	
-	#Create the wp-config.php file
-	sudo -u $USER -i -- wp core config --path=$INSTALLPATH/$installname/public_html --dbhost=$DBHOST --dbname=wp_$installname --dbuser=$DBUSER --dbpass=$DBPASS
-	
-	#Install WordPress
-	sudo -u $USER -i -- wp core install --path=$INSTALLPATH/$installname/public_html --url=http://$devdomain --title=$WPTITLE --admin_user=$WPUSER --admin_password=$WPPASS --admin_email=$serveradmin	
+	sudo -u $USER -i -- wp core download --path=$INSTALLPATH/$FOLDER/$installname/public_html
+
+	chmod -R 777 $INSTALLPATH/$FOLDER/$installname
+
+	sudo -u $USER -i -- wp core config --path=$INSTALLPATH/$FOLDER/$installname/public_html --dbhost=$DBHOST --dbname=wp_$installname --dbuser=$DBUSER --dbpass=$DBPASS
+
+	sudo -R 777 $INSTALLPATH/$installname
+
+	sudo -u $USER -i -- wp core install --path=$INSTALLPATH/$FOLDER/$installname/public_html --url=http://$DEVDOMAIN --title=$WPTITLE --admin_user=$WPUSER --admin_password=$WPPASS --admin_email=$serveradmin
 
 	echo "WordPress installation complete."
 fi
